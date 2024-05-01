1d2c82e0d38a0c4af75a1221421e2199aaf9bc84
kedabudre <abdurehimk77@gmail.com>
2024-05-02 00:52:43 +0300

    update file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,9 @@
 -- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
