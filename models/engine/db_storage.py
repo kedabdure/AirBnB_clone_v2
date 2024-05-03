982389f5e57b609915d81e73de7c5a1186ed74d5
kedabudre <abdurehimk77@gmail.com>
2024-05-03 23:10:32 +0300

    update some file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
 from os import getenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from base_model import Base
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage:
@@ -19,8 +28,43 @@ class DBStorage:
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
             
-        def all(self, cls=None)
+        def all(self, cls=None):
             """query on the current db session"""
+            self.__session = sessionmaker(bind=self.__engine)
             if cls is None:
-                session.query(cls)
-            
+                obj = self.__session.query(State).all()
+                obj.extend(self.__session.query(User).all())
+                obj.extend(self.__session.query(Place).all())
+                obj.extend(self.__session.query(City).all())
+                obj.extend(self.__session.query(Amenity).all())
+                obj.extend(self.__session.query(Review).all())
+            else:
+                if type(cls) == str:
+                    cls = eval(cls)
+                objs = cls.__session.query(cls).all()
+                
+            return {"{}.{}".format(obj, obj.id): obj for obj in objs}
+
+    def new(self, obj):
+        """add the obj to the current database"""
+        self._session.add(obj)
+
+    def save(self, obj):
+        """commit the change to the database"""
+        self.__session.commit(obj)
+
+    def delete(self, obj=None):
+        """delete from database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """create all tables in the database"""
+        Base.metadata.create_all(self.__engine)
+        sussion_fac = sessionmaker(self.__engine, expire_on_commit=False)
+        Session = scoped_session(sussion_fac)
+        self.__session = Session()
+
+    def close(self):
+        """Close the working sqlalchemy session"""
+        self.__session.close()
