622c9022ca711c026990bd9b4322df8b0bf1dc24
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-05-17 23:26:04 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,8 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
-
+env.hosts = ["52.7.16.87", "34.229.56.62"]
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
